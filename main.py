523af4ca1410a29e9c7a089f7db2efd1a5ce6b12
Mohammad <khalisrasuli@gmai.com>
2022-03-08 10:40:02 +0000

    Added first part

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 
+# Read how many employees you need to enter?
+employee_count_str = input("please enter how many employees you have: ")
+employee_count = int(employee_count_str)
+for employee_index in range (employee_count):
+# loop through employees
+# Read first name, last name, and age
+    first_name = input("please enter the first name: ")
+    last_name = input("please enter your last name: ")
+    age_str = input("please enter your age: ")
+    age = int(age_str)
+    print(f"Employee data: First Name: {first_name}, Last Name: {last_name}, Age: {age}")
+#
+
+
+# check first name and last name are not empty
+
+# check that employees are in between age 18 and 100
+
+# Check the total age is not more than 500
